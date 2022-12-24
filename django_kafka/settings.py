3024d4cffb5479e0a5967d88d134ddf2148f8504
phankieuphu <phankieuphu@gmail.com>
2022-12-24 17:49:21 +0700

    fix serect

--- django_kafka/settings.py
+++ django_kafka/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gb2eg&c0w%ft7og+!!d7^#+e*^k0+jmk8nsw+auz(#oh^n7bvq'
+# SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
